96fca423c9e1fde675283923cd74f96a48e8719b
Bryan Roof <broof716@gmail.com>
2021-04-14 17:55:02 -0700

    creating Facebook Clone with MERN stack

---
+++ src/components/Header.js
@@ -0,0 +1,71 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  Add,
+  ExpandMore,
+  Flag,
+  Forum,
+  Home,
+  NotificationsActive,
+  Search,
+  StorefrontOutlined,
+  Subscriptions,
+  SupervisedUserCircle,
+} from "@material-ui/icons";
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"
+          alt="Facebook logo"
+        />
+      </div>
+
+      <div className="header__input">
+        <Search />
+        <input placeholder="Search Facebook" type="text" />
+      </div>
+      <div className="header__center">
+        <div className="header__option header__option--active">
+          <Home fontSize="large" />
+        </div>
+        <div className="header__option">
+          <Flag fontSize="large" />
+        </div>
+        <div className="header__option">
+          <Subscriptions fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlined fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircle fontSize="large" />
+        </div>
+      </div>
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4>Bryan Roof</h4>
+        </div>
+
+        <IconButton>
+          <Add />
+        </IconButton>
+        <IconButton>
+          <Forum />
+        </IconButton>
+        <IconButton>
+          <NotificationsActive />
+        </IconButton>
+        <IconButton>
+          <ExpandMore />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
